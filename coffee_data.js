1a074efb02b18ca4eb5298612302478bfefe8cf6
dtill8945 <dtill8945@gmail.com>
2023-05-02 16:24:11 -0500

    edited coffee_data file

--- coffee_data.js
+++ coffee_data.js
@@ -10,47 +10,47 @@
 const coffeeMenu = [
     {
         name: "cappuccino",
-        price: "8",
+        price: 8.00,
         seasonal: false,
     },
     {
-        name: espresso,
-        price: 5,
+        name: "espresso",
+        price: 5.00,
         seasonal: false,
-    }
+    },
     {
         name: "latte",
-        price: 5,
+        price: 5.00,
         seasonal: false,
     },
     {
-        name: affogato,
-        price: "9",
+        name: "affogato",
+        price: 9.00,
         seasonal: true,
     }, 
     {
         name: "macchiato",
-        price: "6",
-        seasonal: false,
-    }
-    {
-        name: americano,
-        price: 7,
-        seasonal: false,
-    }
-    {
-        name: "iced coffee",
-        price: "6",
+        price: 6.00,
         seasonal: false,
     },
     {
-        name: frappe,
-        price: "8",
+        name: "americano",
+        price: 7.00,
         seasonal: false,
-    }
+    },
+    {
+        name: "iced coffee",
+        price: 6.00,
+        seasonal: false,
+    },
+    {
+        name: "frappe",
+        price: 8.00,
+        seasonal: false,
+    },
     {
         name: "cuban espresso",
-        price: 10,
+        price: 10.00,
         seasonal: true,
     }
 ]
