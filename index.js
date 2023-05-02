afa6248120217fa6a060f2aeb8f9ccabd5a97740
dtill8945 <dtill8945@gmail.com>
2023-05-02 17:31:46 -0500

    Finished adding pseudo code and logic

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const coffeeMenu = require("./coffee_data.js");
+let coffeeMenu = require("./coffee_data.js"); //Linking our coffee_data.js file
 
-console.log(coffeeMenu[name])
+const coffeeNames = coffeeMenu.map(coffee => coffee.name); //.map will return with only the names
+console.log(coffeeNames)
+
+const cheaperDrinks = coffeeMenu.filter(coffee => coffee.price <= 5); //.filter will return anything less than 5 and then we use .map to return the names of these coffee's under 5
+const cheaperDrinkNames = cheaperDrinks.map(coffee => coffee.name);
+console.log(cheaperDrinkNames)
+
+const evenDrinks = coffeeMenu.filter(coffee => coffee.price % 2 === 0); //.filter will return only even costing coffee's and .map will return these names
+const evenDrinkNames = evenDrinks.map(coffee => coffee.name);
+console.log(evenDrinkNames)
+
+const sumOfDrinks = coffeeMenu.reduce((sum, coffee) => sum + coffee.price, 0); //using .reduce we can add the sum of the coffee.price key
+console.log("Your total will be $",sumOfDrinks)
+
+const seasonalDrinks = coffeeMenu.filter(coffee => coffee.seasonal === true); //.filter will take the two which are true for being seasonal and then .map these with a string to give us the correct output
+const seasonalDrinkNames = seasonalDrinks.map(coffee => coffee.name + " with imported beans");
+console.log(seasonalDrinkNames)
